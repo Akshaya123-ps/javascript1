e5ca31352306d99d466271782cb0c619c32da19f
Akshaya123-ps <144515328+Akshaya123-ps@users.noreply.github.com>
2024-07-18 22:23:43 +0530

    script3.js

--- A1/script3.js
+++ A1/script3.js
@@ -1,1 +1,30 @@
+function isPrime(r) {
+    if (r <= 1) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(r); i++) {
+        if (r % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
+function FirstElement() {
+    const inputArr = prompt("Enter an array of numbers (comma-separated):");
+    const array = inputArr.split(",").map(Number);
+
+    if (array.length === 0) {
+        console.log("Array is empty.");
+        return;
+    }
+
+    const firstElement = array[0];
+    if (isPrime(firstElement)) {
+        console.log(`${firstElement} is a prime number.`);
+    } else {
+        console.log(`${firstElement} is not a prime number.`);
+    }
+}
+
+FirstElement();
